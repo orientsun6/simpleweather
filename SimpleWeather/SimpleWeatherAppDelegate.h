36af4518df6aacaa916236b9a35a70a50c569014
Charles Liu <dongyangliu@dongyangs-MacBook-Air.local>
2014-08-27 15:34:06 -0400

    Initial Commit

---
+++ SimpleWeather/SimpleWeatherAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SimpleWeatherAppDelegate.h
+//  SimpleWeather
+//
+//  Created by Charles Liu on 2014-08-27.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleWeatherAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleWeather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleWeather
+//
+//  Created by Charles Liu on 2014-08-27.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SimpleWeatherAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SimpleWeatherAppDelegate class]));
+    }
+}
